9ccb22ef13d0a4e3662a6365104cc395613383b7
Dhruv Datta <136216533+Dhruv-Datta@users.noreply.github.com>
2023-06-14 22:26:23 -0500

    fixed path

--- server_connection.ps1
+++ server_connection.ps1
@@ -1,7 +1,7 @@
 # Register-ScheduledTask -Task $task -ComputerName $remoteCompute
 
 $remoteComputer = "CTO APPS17A"
-$filePath = "C:\Dhruv\main_CTO-APPS17A.exe"
+$filePath = "C:\Dhruv\Desktop\main_CTO-APPS17A.exe"
 
 $scriptBlock = {
     param($filePath)
